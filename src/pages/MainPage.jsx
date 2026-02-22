c3df1fc11e0f6877975dadf80efe81a698248326
Minho Kang <busster@naver.com>
2026-02-22 21:05:10 +0900

    로그아웃 테스트 / 로그아웃 시, 액세스,리프레시 토큰 삭제되는 것 확인

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,9 +1,26 @@
+import { useNavigate } from "react-router";
 
-const MainPage = ()=>{
 
-    return(<>
-    <h1>메인페이지</h1>
-    </>)
-}
+
+const MainPage = () => {
+
+    const nav = useNavigate();
+
+    const handleLogout = async () => {
+        await fetch("http://localhost:8080/api/logout", {
+            method: "POST",
+            credentials: "include" // 쿠키 전송
+        });
+        alert("로그아웃 완료!");
+        nav('/') // 로그인 페이지로 이동
+    };
+
+    return (
+        <>
+        <h1>메인페이지</h1>
+        <button onClick={handleLogout}>로그아웃</button>
+        </>
+    );
+};
 
 export default MainPage;
